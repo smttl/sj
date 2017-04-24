7fabcaa061728ed058b3edb76798e89c1206e983
Samet ışık <sametreiz123asdf@gmail.com>
2017-04-24 22:18:03 +0300

    Add files via upload

---
+++ Assets/don.cs
@@ -0,0 +1,19 @@
+﻿
+	using UnityEngine;
+	using System.Collections;
+
+	public class don : MonoBehaviour 
+	{
+		float rotSpeed = 100;
+
+		void OnMouseDrag()
+		{
+			float rotX = Input.GetAxis("Mouse X")*rotSpeed*Mathf.Deg2Rad;
+			float rotY = Input.GetAxis("Mouse Y")*rotSpeed*Mathf.Deg2Rad;
+
+			transform.RotateAround(Vector3.up, -rotX);
+			transform.RotateAround(Vector3.down, rotY);
+		}
+	}
+
+
